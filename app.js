5dd40f98bd4ff2ebbe4f3ea3cf8ce83bc68d8a4d
Rahul Sharma <rahulpancholi3937@gmail.com>
2023-03-20 19:27:24 +0530

    updates

--- app.js
+++ app.js
@@ -9,15 +9,35 @@ app.use(express.json());
 
 app.post("/students",(req,res)=>{
 console.log(req.body)
-    const user = new Student(req.body)
+
+// const user =  Student.findOne(req.body.email)
+
+   
+const user = new Student(req.body)
+
     user.save().then(()=>{
         res.status(200).send(user)
-
+    
     }).catch((e)=>{
-    res.send(e)
-
+    res.send("Please try wtih diffrent email")
+    
     })
+
+
+
+
+  
+
+
+
+    
+   
 })
+
+
+
+
+
 app.get("/students",async (req,res)=>{
 
     try{
@@ -43,7 +63,7 @@ app.get("/students/:id",async (req,res)=>{
 app.delete("/students/remove/:id",async (req,res)=>{
 
     try{
-        const Siddata = await Student.findByIdAndRemove(req.params.id);
+         await Student.findByIdAndRemove(req.params.id);
         res.send("deleted")
     }catch{
         res.send("eroor")
